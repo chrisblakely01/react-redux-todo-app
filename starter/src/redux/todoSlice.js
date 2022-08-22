f030617df86771c7566ff1c5652b2a4e6c681996
PKG-7 <pavel.k.group@gmail.com>
2022-08-22 18:34:22 +0300

    delete works now

--- starter/src/redux/todoSlice.js
+++ starter/src/redux/todoSlice.js
@@ -19,10 +19,13 @@ const todoSlice = createSlice({
         toggleComplete: (state, action) => {
             const index = state.findIndex((todo)=> todo.id === action.payload.id );
             state[index].completed = action.payload.completed;
+        },
+        deleteTodo: (state, action) => {
+            return state.filter((todo) => todo.id !== action.payload.id)
         }
     },
 });
 
-export const { addTodo , toggleComplete } = todoSlice.actions;
+export const { addTodo , toggleComplete , deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
