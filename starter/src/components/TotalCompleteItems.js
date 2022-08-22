6e6b981e8c02461745585b5ed356eccd8689daee
PKG-7 <pavel.k.group@gmail.com>
2022-08-22 18:56:19 +0300

    all completed and working (no async)

--- starter/src/components/TotalCompleteItems.js
+++ starter/src/components/TotalCompleteItems.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+
 
 const TotalCompleteItems = () => {
-	return <h4 className='mt-3'>Total Complete Items: 5</h4>;
+	const completedTodos= useSelector((state)=> state.todos.filter((todo) => todo.completed === true))
+	
+	return <h4 className='mt-3'>Total Complete Items: {completedTodos.length}</h4>;
 };
 
 export default TotalCompleteItems;
