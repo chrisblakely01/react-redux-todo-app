a75e582c6410b3a84506ac3e4b72fd057740fb20
PKG-7 <pavel.k.group@gmail.com>
2022-08-23 22:23:13 +0300

    tre add todo async

--- frontend/src/redux/todoSlice.js
+++ frontend/src/redux/todoSlice.js
@@ -9,6 +9,24 @@ async () => {
     }
 })
 
+export const addTodoAsync = createAsyncThunk(
+    'todos/todoAsync',
+     async(payload) => {
+        const response = await fetch('http://localhost:7000/todos', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({title: payload.title})
+        });
+
+        if(response.ok) {
+            const todo = await response.json();
+            return { todo };
+        }
+    }
+);
+
 const todoSlice = createSlice({
     name :"todos",
     initialState: [
@@ -40,6 +58,9 @@ const todoSlice = createSlice({
         [getTodosAsync.fulfilled]: (state, action) => {
             console.log('Fettched data successfully!');
             return action.payload.todos;
+        },
+        [addTodoAsync.fulfilled]: (state, action) => {
+            state.push(action.payload.todo);
         }
     }
 });
